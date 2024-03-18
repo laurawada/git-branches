52a38c26ae538497d0c4aba951909a921bc102da
BrunaCCardoso <bruna.cardoso@germinare.org.br>
2024-03-18 07:35:40 -0300

    Alterando e comittando a classe-cliente

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,8 @@
 public class Cliente {
     private String nome;
+    // alterando o nome String privado
     private String cpf;
     private int agencia;
     private double saldo;
+    private int b;
 }
